61924ed902c74fe38b6cfdbb8eaa915772106be7
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2023-10-12 13:00:23 -0300

    @Reynald-Albuquerque-Costa[add ex001]

---
+++ ex001.py
@@ -0,0 +1,10 @@
+""" Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D """
+
+A = int(input ())
+B = int(input ())
+C = int(input ())
+D = int(input ())
+
+diferenca = (A * B - C * D)
+
+print(f'DIFERENCA = {diferenca}')
